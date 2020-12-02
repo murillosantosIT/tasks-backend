284bb84049f70377ffea1235e99800bd7188e61d
DESKTOP-06QM5F9\muril <murillo.santos100@gmail.com>
2020-12-01 23:28:51 -0300

    adicionando testes unitários TaskController e DateUtils

---
+++ src/test/java/br/ce/wcaquino/taskbackend/utils/DateUtilsTest.java
@@ -0,0 +1,30 @@
+package br.ce.wcaquino.taskbackend.utils;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.time.LocalDate;
+
+public class DateUtilsTest {
+
+    @Test
+    public void deveRetornarTrueParaDatasFuturas(){
+        LocalDate date = LocalDate.of(2030,01,01);
+        Assert.assertTrue(DateUtils.isEqualOrFutureDate(date));
+        System.out.println(DateUtils.isEqualOrFutureDate(date));
+    }
+
+    @Test
+    public void deveRetornarFalseParaDatasFuturas(){
+        LocalDate date = LocalDate.of(2010,01,01);
+        Assert.assertFalse(DateUtils.isEqualOrFutureDate(date));
+        System.out.println(DateUtils.isEqualOrFutureDate(date));
+    }
+
+    @Test
+    public void deveRetornarTrueParaDataAtual(){
+        LocalDate date = LocalDate.now();
+        Assert.assertTrue(DateUtils.isEqualOrFutureDate(date));
+        System.out.println(DateUtils.isEqualOrFutureDate(date));
+    }
+}
